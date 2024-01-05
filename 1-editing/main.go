20b15107ca78efb74f81c59ae8dc6ec3f28d2e20
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-01-05 09:51:15 +0900

    標準ライブラリを使ってwebサーバーを実装

--- 1-editing/main.go
+++ 1-editing/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+type myHandler struct{}
+
+func (h *myHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "Serve HTTP")
+}
 
 func main() {
 	fmt.Println("Hello World")
+
+	handler := &myHandler{}
+	http.Handle("/", handler)
+	// http.HandleFunc("/", handler.ServeHTTP)
+	http.ListenAndServe(":8080", nil)
 }
