da4601a318670f1da2917d2f490be81157394f8f
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-01-12 17:09:43 +0900

    メソッド呼び出しについて学習したことをコメントに追記した

--- 2/main.go
+++ 2/main.go
@@ -15,6 +15,12 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
+	// メソッドのレシーバーがポインタ型で定義されている場合、呼び出し時にそのオブジェクト（レシーバー）が値型であっても呼び出すことは可能。
+	// レシーバーが値型で定義されていて、呼び出し時はポインタ型のオブジェクト（レシーバー）でも同様に呼び出せる
+	// ただし揃えないと気持ち悪いし、挙動の誤解を起こしそう...
+
+	// taskController := controller.TaskController{}
+
 	taskController := &controller.TaskController{}
 
 	e.GET("/tasks", taskController.Get)
