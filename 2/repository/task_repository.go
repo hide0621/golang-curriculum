544dbe44b1b01d4732cdd0513219564c776e512f
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-01-15 09:44:31 +0900

    レポジトリー層にてCRUDのうちCreateを実装した

--- 2/repository/task_repository.go
+++ 2/repository/task_repository.go
@@ -3,7 +3,7 @@ package repository
 import "database/sql"
 
 type TaskRepository interface {
-	Create(task *Task) error
+	Create(task *Task) (int, error)
 	Read(id int) (*Task, error)
 	Update(task *Task) error
 	Delete(id int) error
@@ -13,6 +13,22 @@ type taskRepositoryImpl struct {
 	db *sql.DB
 }
 
+// コントローラー層に実装されていて２重実装になるが、一旦ここにも実装
+type Task struct {
+	ID    int    `json:"id"`
+	Title string `json:"title"`
+}
+
 func NewTaskRepository(db *sql.DB) TaskRepository {
 	return &taskRepositoryImpl{db: db}
+}
+
+func (r *taskRepositoryImpl) Create(task *Task) (int, error) {
+
+	// ここでSQLを実行
+	stmt := `INSERT INTO tasks (title) VALUES (?) RETURNING id`
+	err := r.db.QueryRow(stmt, task.Title).Scan(&task.ID)
+
+	return task.ID, err
+
 }
