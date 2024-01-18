660a5f3b9fc1a450652b18af83127f57bf11b94f
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-01-18 14:24:31 +0900

    Divide関数に標準パッケージによるテーブルドリブンテストを実装した

--- 3/calculator_test.go
+++ 3/calculator_test.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
 func TestAdd(t *testing.T) {
@@ -14,6 +12,12 @@ func TestAdd(t *testing.T) {
 	}
 }
 
+type divideTestCase struct {
+	a, b           int
+	expectedResult int
+	expectErr      bool
+}
+
 func TestSubtract(t *testing.T) {
 	result := Subtract(2, 1)
 	expected := 1
@@ -22,19 +26,45 @@ func TestSubtract(t *testing.T) {
 	}
 }
 
+// func TestDivide(t *testing.T) {
+
+// 	result, err := Divide(6, 3)
+
+// 	assert.NoError(t, err, "an unexpected error occurred: %v", err)
+// 	assert.Equal(t, 2, result, "they should be equal")
+
+// 	if err != nil {
+// 		t.Errorf("Divide(6, 3) failed. Expected no error, got %v", err.Error())
+// 	}
+// 	expected := 2
+// 	if result != expected {
+// 		t.Errorf("Divide(6, 3) returned %d, expected 2.", result)
+// 	}
+
+// }
+
+// テーブルドリブンテスト 標準パッケージver
 func TestDivide(t *testing.T) {
 
-	result, err := Divide(6, 3)
+	testCases := []divideTestCase{
+		{a: 6, b: 3, expectedResult: 2, expectErr: false},
+		{a: 6, b: 0, expectedResult: 0, expectErr: true},
+	}
 
-	assert.NoError(t, err, "an unexpected error occurred: %v", err)
-	assert.Equal(t, 2, result, "they should be equal")
-
-	// if err != nil {
-	// 	t.Errorf("Divide(6, 3) failed. Expected no error, got %v", err.Error())
-	// }
-	// expected := 2
-	// if result != expected {
-	// 	t.Errorf("Divide(6, 3) returned %d, expected 2.", result)
-	// }
+	for _, tc := range testCases {
+		result, err := Divide(tc.a, tc.b)
+		if tc.expectErr {
+			if err == nil {
+				t.Errorf("Divide(%d, %d) failed. Expected an error, got nil.", tc.a, tc.b)
+			}
+		} else {
+			if err != nil {
+				t.Errorf("Divide(%d, %d) failed. Expected no error, got %v", tc.a, tc.b, err.Error())
+			}
+		}
+		if result != tc.expectedResult {
+			t.Errorf("Divide(%d, %d) returned %d, expected %d.", tc.a, tc.b, result, tc.expectedResult)
+		}
+	}
 
 }
