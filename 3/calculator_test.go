be81b66d5250f1041725b2ff4a632ebdb8d0d12b
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-01-18 10:00:42 +0900

    OSアップデートによるgitの不具合が出たので、Subtract関数とそのテスト関数を実装して、pushが出来るか検証

--- 3/calculator_test.go
+++ 3/calculator_test.go
@@ -9,3 +9,11 @@ func TestAdd(t *testing.T) {
 		t.Errorf("Add(1, 2) returned %d, expected 3.", result)
 	}
 }
+
+func TestSubtract(t *testing.T) {
+	result := Subtract(2, 1)
+	expected := 1
+	if result != expected {
+		t.Errorf("Subtract(2, 1) returned %d, expected 1.", result)
+	}
+}
