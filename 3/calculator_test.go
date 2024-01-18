eb62404b38cab288e28049c0b6cc93f72081b56f
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-01-18 10:34:18 +0900

    Divide関数のテストを作成した

--- 3/calculator_test.go
+++ 3/calculator_test.go
@@ -17,3 +17,14 @@ func TestSubtract(t *testing.T) {
 		t.Errorf("Subtract(2, 1) returned %d, expected 1.", result)
 	}
 }
+
+func TestDivide(t *testing.T) {
+	result, err := Divide(6, 3)
+	if err != nil {
+		t.Errorf("Divide(6, 3) failed. Expected no error, got %v", err.Error())
+	}
+	expected := 2
+	if result != expected {
+		t.Errorf("Divide(6, 3) returned %d, expected 2.", result)
+	}
+}
