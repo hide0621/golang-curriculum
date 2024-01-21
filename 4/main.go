d7afbf35d431168831a5a92b78b9c3c935eb7cd0
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-01-22 08:50:37 +0900

    「ポインタとデリファレンス」で学んだことについてコメントを追記

--- 4/main.go
+++ 4/main.go
@@ -9,9 +9,9 @@ func main() {
 	fmt.Println("numのアドレス:", &num) //numのアドレス: 0x1400009c018
 	fmt.Println("numの値:", num)     //numの値: 10
 
-	var ptr *int = &num //numのアドレスをptrに代入
+	var ptr *int = &num //numのアドレスをptrに代入 (変数numのアドレスを格納する変数ptrを宣言)
 
-	fmt.Println("ptrが指し示すアドレス:", ptr) //ptrが指し示すアドレス: 0x1400009c018
+	fmt.Println("ptrが指し示すアドレス:", ptr) //ptrが指し示すアドレス: 0x1400009c018 (numのアドレスと同じ) ← ここがポイント
 	fmt.Println("ptrが指し示す値:", *ptr)   //ptrが指し示す値: 10
 	fmt.Println("ptr自体のアドレス:", &ptr)  //ptr自体のアドレス: 0x1400009c020
 
