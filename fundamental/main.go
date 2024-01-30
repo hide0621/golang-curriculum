1b5a884bc1eaf182373790522ea0c80d5162720a
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-01-30 17:16:04 +0900

    色んなif文のパターンを簡潔に書いてみた

--- fundamental/main.go
+++ fundamental/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func main() {
-	fmt.Println("fundamental")
+
+	i := 10
+
+	if i == 10 {
+		fmt.Println("i is 10")
+	} else {
+		fmt.Println("i is not 10")
+	}
+
+	if true {
+		fmt.Println("true")
+	} else {
+		fmt.Println("false")
+	}
+
+	// 上と下のif文は同じ意味
+
+	ok := true
+
+	if ok {
+		fmt.Println("true")
+	}
+
+	if !ok {
+		fmt.Println("false")
+	}
+
+	if err := errors.New("error"); err != nil {
+		fmt.Println(err)
+	}
+
 }
