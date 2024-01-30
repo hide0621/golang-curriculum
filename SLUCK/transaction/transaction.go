3e270bcd798b56da1de02e1e92079b113220e521
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-01-30 09:44:24 +0900

    DoInTxメソッドについて完全に実装した

--- SLUCK/transaction/transaction.go
+++ SLUCK/transaction/transaction.go
@@ -17,7 +17,40 @@ func NewTransaction(db *sql.DB) Transaction {
 	return &tx{db: db}
 }
 
+// 引数fにはDB操作を行う関数を渡す
 func (t *tx) DoInTx(ctx context.Context, f func(context.Context) (any, error)) (any, error) {
 
-	return nil, nil
+	// トランザクションを開始する（無名のトランザクションのオブジェクトの作成）
+	tx, err := t.db.BeginTx(ctx, &sql.TxOptions{})
+	if err != nil {
+		return nil, err
+	}
+
+	/*
+		上記で作成した無名のトランザクションに対して「tx」というキーを与えてコンテキストを作成する
+		ctxの中にはtxトランザクションが入っている
+	*/
+	ctx = context.WithValue(ctx, "tx", tx)
+
+	/*
+		上記で作成したtxトランザクションでDB操作を行う
+		txトランザクション中にエラーが発生した場合はロールバックする
+	*/
+	v, err := f(ctx)
+	if err != nil {
+		tx.Rollback()
+		return nil, err
+	}
+
+	/*
+		上記でエラーが発生しなかった場合はコミットする
+		コミット出来なかった場合はロールバックする
+	*/
+	err = tx.Commit()
+	if err != nil {
+		tx.Rollback()
+		return nil, err
+	}
+
+	return v, nil
 }
